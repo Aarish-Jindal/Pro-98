134cf0a0d1efabc9c0d5ba26e88e6ce6a93ef1f8
Aarish-Jindal <69344631+Aarish-Jindal@users.noreply.github.com>
2022-01-11 10:01:52 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,13 +1,9 @@
-# File swpper
-# Made by somePythonProgrammer as a WhiteHat project.
 
-# read the files
-global file1_contents,file2_contents
+file1_contents,file2_contents
 with open('file_1.txt') as f1, open('file_2.txt') as f2:
     file1_contents = f1.read()
     file2_contents = f2.read()
 
-# write the files, swapping them.
 open('file_1.txt','w').write(file2_contents)
 open('file_2.txt','w').write(file1_contents)
 
